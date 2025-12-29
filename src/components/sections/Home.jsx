3dbd0e17eca8517026a99bb496e43eef9b2eb8eb
KentDoCode <villalunkent03@gmail.com>
2025-12-29 14:39:11 +0800

    Describe changes

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,7 +5,6 @@ import { LinkedinIcon } from "../icons/LinkedinIcon";
 import { useTypewriter } from "react-simple-typewriter";
 import { RevealOnScroll } from "../RevealOnScroll";
 
-
 export const Home = ({ isLoaded }) => {
   const [text] = useTypewriter({
     words: ["n aspiring developer.", " student leader.", " creative designer."],
@@ -18,7 +17,6 @@ export const Home = ({ isLoaded }) => {
       id="home"
       className="min-h-screen flex items-center justify-center relative bg-[#060A14]"
     >
-      
       {/* Gradient Background */}
       <div
         className={`absolute top-0 left-1/2 -translate-x-1/2 md:-translate-y-1/3 w-screen h-screen gradient-background transition-all duration-1000 ease-out pointer-events-none
@@ -31,7 +29,6 @@ export const Home = ({ isLoaded }) => {
       ></div>
 
       <RevealOnScroll>
-        
         <div className="z-10 text-center flex flex-col gap-9.25 mx-auto">
           <div className="">
             <span className="text-xl md:text-2xl transition-all">Hey! I'm</span>
@@ -53,7 +50,10 @@ export const Home = ({ isLoaded }) => {
               <DownloadIcon />
               <span className="">Download Resume</span>
             </button>
-            <a href="#contact" className="flex flex-row gap-3.25 py-4 px-10.5 rounded-lg bg-[#3B82F6] hover:bg-[#0876e4] transition-all cursor-pointer hover:-translate-y-0.5 hover:shadow-[0,0,15px_rgba(59, 130, 246, 0.5)] font-semibold">
+            <a
+              href="#contact"
+              className="flex flex-row gap-3.25 py-4 px-10.5 rounded-lg bg-[#3B82F6] hover:bg-[#0876e4] transition-all cursor-pointer hover:-translate-y-0.5 hover:shadow-[0,0,15px_rgba(59, 130, 246, 0.5)] font-semibold"
+            >
               <MailIcon />
               <span className="">Get in Touch</span>
             </a>
