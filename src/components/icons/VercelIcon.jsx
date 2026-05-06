401859836b6e180e522a65fdaccfa5bff8fbdaf5
KentDoCode <villalunkent03@gmail.com>
2026-05-06 19:44:37 +0800

    Technologies section

---
+++ src/components/icons/VercelIcon.jsx
@@ -0,0 +1,9 @@
+export const VercelIcon = ({className, fill}) => {
+  return (
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={className}
+      fill={fill}>
+      <title>Vercel</title>
+      <path d="m12 1.608 12 20.784H0Z" />
+    </svg>
+  );
+};
--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -7,58 +7,82 @@ import { ReactIcon } from "../icons/ReactIcon";
 import { TailwindIcon } from "../icons/TailwindIcon";
 import { GitIcon } from "../icons/GitIcon";
 import { NetlifyIcon } from "../icons/NetlifyIcon";
+import { NextIcon } from "../icons/NextIcon";
+import { PostgresIcon } from "../icons/PostgresIcon";
+import { Prismaicon } from "../icons/PrismaIcon";
+import { VercelIcon } from "../icons/VercelIcon";
+import { ExpressIcon } from "../icons/ExpressIcon";
+import { NodeIcon } from "../icons/NodeIcon";
+
 import { RevealOnScroll } from "../RevealOnScroll";
 import { Meteors } from "../ui/Meteors";
+import { SpotlightCard } from "../ui/SpotlightCard";
+import { BorderGlow } from "../ui/BorderGlow";
+import { useState } from "react";
 
 export const Skills = () => {
+
+  const [hovered, setHovered] = useState(false)
+
   const technologies = [
     { name: "HTML", Icon: HtmlIcon, color: "#E34F26" },
     { name: "CSS", Icon: CssIcon, color: "#663399" },
     { name: "JavaScript", Icon: JsIcon, color: "#F7DF1E" },
     { name: "React.js", Icon: ReactIcon, color: "#61DAFB" },
+    { name: "Next.js", Icon: NextIcon, color: "#000000" },
+    { name: "Node.js", Icon: NodeIcon, color: "#5FA04E" },
+    { name: "Express.js", Icon: ExpressIcon, color: "#000000" },
     { name: "Tailwind CSS", Icon: TailwindIcon, color: "#06B6D4" },
-    { name: "Github", Icon: GithubFilledIcon, color: "#181717" },
+    { name: "PostgreSQL", Icon: PostgresIcon, color: "#4169E1" },
+    { name: "Prisma", Icon: Prismaicon, color: "#2D3748" },
+    { name: "Vercel", Icon: VercelIcon, color: "#000000" },
     { name: "Git", Icon: GitIcon, color: "#F05032" },
-    { name: "Netlify", Icon: NetlifyIcon, color: "#00C7B7" },
-    { name: "PHP", Icon: PhpIcon, color: "#777BB4" },
   ];
 
   return (
     <>
       <section
-        className="min-h-[50%] flex items-center justify-center py-20 relative"
+        className="min-h-[70%] flex items-center justify-center py-20 relative"
         id="skills"
       >
-        <Meteors number={20}/>
+        <Meteors number={20} />
         <RevealOnScroll>
-          <div className="mx-auto px-6 max-w-3xl md:max-w-5xl">
+          <div className="mx-auto px-6 lg:px-0 max-w-3xl md:max-w-5xl">
             <div className="mb-11.25 text-center">
               <h2 className="font-bold text-3xl md:text-4xl mb-8">
                 Technologies
               </h2>
+              <p className="text-lg">
+                Languages, frameworks, and tools I work with.
+              </p>
+            </div>
 
-              <div className="relative overflow-hidden px-6">
-                <div className="flex animate-marquee ">
-                  {[...technologies, ...technologies, ...technologies].map(
-                    ({ name, Icon, color, key }) => (
-                      <div
-                        className="shrink-0 md:shrink py-4 mr-10 group z-50 "
-                        key={key}
-                      >
-                        <div className="">
-                          <Icon
-                            className={`md:w-24 w-15 opacity-100 md:opacity-45 md:hover:opacity-100 transition-all cursor-pointer`}
-                            fill={color}
-                          />
-                        </div>
-                      </div>
-                    )
-                  )}
-                </div>
+            <div className="grid grid-cols-3 gap-2 md:gap-3 md:grid-cols-6">
+              {technologies.map(({ name, Icon, color }) => (
 
-                <div className="pointer-events-none absolute inset-y-0 left-0 w-25 bg-linear-to-r from-[#060A14] to-transparent z-20" />
-                <div className="pointer-events-none absolute inset-y-0 right-0 w-25 bg-linear-to-l from-[#060A14] to-transparent z-20" />
-              </div>
+                  <BorderGlow
+                    edgeSensitivity={40}
+                    glowColor="40 80 80"
+                    backgroundColor="#0A0F1E"
+                    borderRadius={16}
+                    glowRadius={59}
+                    glowIntensity={2.2}
+                    coneSpread={29}
+                    animated={false}
+                    colors={["#3B82F6", "#6366F1", "#8B5CF6"]}
+                    className="p-1 " 
+                  >
+                    <div className="flex items-center flex-col py-3 px-8 hover:bg-[#0D1425] gap-1 bg-[#0A0F1E] rounded-xl transition-all">
+                      <Icon className={`w-10 z-50`} fill={color} />
+                      <p className="text-xs text-white/70 ">{name}</p>
+                    </div>
+                  </BorderGlow>
+           
+                // <SpotlightCard className="custom-spotlight-card flex items-center justify-center flex-col gap-1" key={name} spotlightColor="#3B82F6" >
+                //   <Icon className={`w-10 z-50`} fill={color} />
+                //   <p className="text-xs text-white/70 ">{name}</p>
+                // </SpotlightCard>
+              ))}
             </div>
           </div>
         </RevealOnScroll>
